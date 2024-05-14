d62ee50e5ae68072fcb4d42c4ff29d58e3be4a83
waqar-husain <waqarhusain246@gmail.com>
2024-05-14 23:31:33 +0530

    FINAL

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -25,11 +25,26 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <Head>
         <link
-          rel="icon"
-          href="./Meubel House_Logos-05.png"
-          type="image/<generated>"
-          sizes="<generated>"
+          rel="apple-touch-icon"
+          sizes="76x76"
+          href="/apple-touch-icon.png"
         />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#f9f1e7" />
+        <meta name="theme-color" content="#f9f1e7" />
       </Head>
       <body className={poppins.className}>
         <StoreProvider>
